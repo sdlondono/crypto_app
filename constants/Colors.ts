e928b4126b1f0ffa64dbead1b4a30112665d4f35
Samuel David Londono <samioff19@gmail.com>
2025-05-09 08:35:48 -0500

    feat(mobile): add colors

---
+++ constants/Colors.ts
@@ -0,0 +1,15 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const Colors = {
+  background: '#121212',
+  card: '#1E1E1E',
+  yellow: '#FCD535',
+  text: '#FFFFFF',
+  mutedText: '#B3B3B3',
+  border: '#2C2C2C'
+}
+
+export default Colors
