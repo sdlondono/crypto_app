56eef2a5c56d51398453e682463cc01612f74829
Samuel David Londono <samioff19@gmail.com>
2025-05-09 08:38:45 -0500

    feat(mobile): add models

---
+++ models/Crypto.ts
@@ -0,0 +1,10 @@
+export interface ICrypto {
+  id: string
+  name: string
+  symbol: string
+  price_usd: string
+  percent_change_24h: string
+  market_cap_usd: string
+  volume24: number
+  rank: number
+}
